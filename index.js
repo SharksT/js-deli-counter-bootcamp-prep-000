a2502563ba5a1959c9b8f26aabceb14d9e3fe097
Felipe Passos <felipekaires@hotmail.com>
2017-10-21 22:55:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var count = 1;
 function takeANumber(array,name) {
   array.push(name);
   var a = array.lenght;
-  var text = ("Welcome, " +  name + ". You are number " + array.lenght + " in line.");
+  var text = ("Welcome, " +  name + ". You are number " + a + " in line.");
   count++;
   return text;
 }
