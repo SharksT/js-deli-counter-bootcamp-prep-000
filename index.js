8998cc8107121d22a76019aa30ff6fc68de8d209
Felipe Passos <felipekaires@hotmail.com>
2017-10-21 22:55:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var katzDeli = [];
-var count = 1;
 function takeANumber(array,name) {
   array.push(name);
   var text = ("Welcome, " +  name + ". You are number " + array.lenght + " in line.");
-  count++;
   return text;
 }
